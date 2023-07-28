7f1b3243f9c0af189e706d199d832f89e73f3278
aojona <aojona@ya.ru>
2023-07-28 15:13:19 +0300

    refactoring: utility logic was moved from `SearchClient` to `QueryUtil`

--- vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/client/SearchClient.java
+++ vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/client/SearchClient.java
@@ -1,15 +1,11 @@
 package ru.kirill.vacancyscanservice.client;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import ru.kirill.vacancyscanservice.annotation.Client;
 import ru.kirill.vacancyscanservice.config.SearchProperties;
 import ru.kirill.vacancyscanservice.model.SearchQuery;
-
-import java.lang.reflect.Field;
-import java.util.Arrays;
+import ru.kirill.vacancyscanservice.util.QueryUtil;
 
 @Client
 @RequiredArgsConstructor
@@ -19,25 +15,8 @@ public class SearchClient {
     private final SearchProperties searchProperties;
 
     public void searchVacancies(SearchQuery query) {
-
-        String url = Arrays
-                .stream(query.getClass().getDeclaredFields())
-                .map(Field::getName)
-                .reduce(
-                        UriComponentsBuilder.fromHttpUrl(searchProperties.url()),
-                        (uriBuilder, name) -> uriBuilder.queryParam(name, getFieldValue(query, name)),
-                        (result, notFinal) -> result
-                )
-                .toUriString();
-
+        String url = QueryUtil.createUrlWithParameters(searchProperties.url(), query);
         String result = restTemplate.getForObject(url, String.class);
         System.out.println(result);
-    }
-
-    @SneakyThrows
-    public String getFieldValue(SearchQuery query, String fieldName) {
-        Field field = query.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        return field.get(query).toString();
     }
 }
--- vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/util/QueryUtil.java
+++ vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/util/QueryUtil.java
@@ -1,2 +1,31 @@
-package ru.kirill.vacancyscanservice.util;public class QueryUtil {
+package ru.kirill.vacancyscanservice.util;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+@UtilityClass
+public class QueryUtil {
+
+    public static <Q> String createUrlWithParameters(String url, Q query) {
+        return Arrays
+                .stream(query.getClass().getDeclaredFields())
+                .map(Field::getName)
+                .reduce(
+                        UriComponentsBuilder.fromHttpUrl(url),
+                        (uriBuilder, name) -> uriBuilder.queryParam(name, getFieldValue(query, name)),
+                        (result, notFinal) -> result
+                )
+                .toUriString();
+    }
+
+    @SneakyThrows
+    private static <Q> Object getFieldValue(Q query, String fieldName) {
+        Field field = query.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(query).toString();
+    }
 }
