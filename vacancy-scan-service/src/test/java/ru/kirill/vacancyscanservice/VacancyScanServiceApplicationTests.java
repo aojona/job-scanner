db6ae68d156ec02a9f4678730b2adca0509fec17
aojona <aojona@ya.ru>
2023-07-28 12:28:48 +0300

    Add vacancy-scan-service

---
+++ vacancy-scan-service/src/test/java/ru/kirill/vacancyscanservice/VacancyScanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill.vacancyscanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyScanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
