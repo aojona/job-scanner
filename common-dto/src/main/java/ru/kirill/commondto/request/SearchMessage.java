00c73ff48f638692bec94b13d8ab25519ff098cb
aojona <aojona@ya.ru>
2023-07-31 18:28:12 +0300

    refactoring: add default constructor for `SearchMessage`

--- common-dto/src/main/java/ru/kirill/commondto/request/SearchMessage.java
+++ common-dto/src/main/java/ru/kirill/commondto/request/SearchMessage.java
@@ -1,10 +1,14 @@
 package ru.kirill.commondto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchMessage {
 
     private String text;
