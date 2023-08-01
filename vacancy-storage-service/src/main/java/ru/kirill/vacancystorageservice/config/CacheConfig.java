262ee94b720384099a641099067b7d27f59a8a2f
aojona <aojona@ya.ru>
2023-08-01 20:03:43 +0300

    feat: `vacancy-storage-service` put vacancies in Redis cache

---
+++ vacancy-storage-service/src/main/java/ru/kirill/vacancystorageservice/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package ru.kirill.vacancystorageservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
--- vacancy-storage-service/src/main/java/ru/kirill/vacancystorageservice/service/VacancyService.java
+++ vacancy-storage-service/src/main/java/ru/kirill/vacancystorageservice/service/VacancyService.java
@@ -1,6 +1,7 @@
 package ru.kirill.vacancystorageservice.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import ru.kirill.vacancystorageservice.entity.VacancyRedis;
 import ru.kirill.vacancystorageservice.repository.VacancyRepository;
@@ -11,7 +12,9 @@ public class VacancyService {
 
     private final VacancyRepository vacancyRepository;
 
+    @Cacheable(cacheNames = "vacancy", key = "#vacancyRedis.id")
     public VacancyRedis saveOrUpdate(VacancyRedis vacancyRedis) {
+        System.out.println("saveOrUpdate:" + vacancyRedis.getId());
         return vacancyRepository.save(vacancyRedis);
     }
 }
