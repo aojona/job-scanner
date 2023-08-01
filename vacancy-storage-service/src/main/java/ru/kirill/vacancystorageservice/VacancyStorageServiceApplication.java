101657f8b4909b1e4b1c909dc86e4a1ef12db4d5
aojona <aojona@ya.ru>
2023-08-01 13:23:01 +0300

    Add `vacancy-storage-service`

---
+++ vacancy-storage-service/src/main/java/ru/kirill/vacancystorageservice/VacancyStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill.vacancystorageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyStorageServiceApplication.class, args);
+	}
+
+}
---
+++ vacancy-storage-service/src/test/java/ru/kirill/vacancystorageservice/VacancyStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill.vacancystorageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
