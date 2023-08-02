e7341f98051b5075ced332d63ac402cb26e990ee
aojona <aojona@ya.ru>
2023-08-02 13:24:15 +0300

    Add `rate-limiter-service`

---
+++ rate-limiter-service/src/test/java/ru/kirill/ratelimiterservice/RateLimiterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill.ratelimiterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
