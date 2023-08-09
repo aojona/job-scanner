61c57a66583facd0c150056a35627e3fdfb829b8
aojona <aojona@ya.ru>
2023-08-09 19:44:14 +0300

    Add `vacancy-notifier-service`

---
+++ vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/VacancyNotifierServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill.vacancynotifierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyNotifierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyNotifierServiceApplication.class, args);
+	}
+
+}
