00cf06d10fa89cd877cb761cbe8d176b49624b5e
aojona <aojona@ya.ru>
2023-08-11 18:47:37 +0300

    refactor: `MemberMapper` correct maps dto to entity now

--- rest-api/src/main/java/ru/kirill/restapi/mapper/MemberMapper.java
+++ rest-api/src/main/java/ru/kirill/restapi/mapper/MemberMapper.java
@@ -1,14 +1,31 @@
 package ru.kirill.restapi.mapper;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ru.kirill.commondto.request.MemberRequest;
 import ru.kirill.commondto.response.MemberResponse;
 import ru.kirill.restapi.entity.Member;
+import ru.kirill.restapi.enums.Role;
+
+import java.util.Optional;
 
 @Component
 public class MemberMapper extends Mapper<Member, MemberRequest, MemberResponse> {
-    public MemberMapper(ModelMapper modelMapper) {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public MemberMapper(ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         super(modelMapper, Member.class, MemberRequest.class, MemberResponse.class);
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void mapToEntity(MemberRequest source, Member destination) {
+        destination.setRole(Role.USER);
+        Optional
+                .of(source.getRawPassword())
+                .map(passwordEncoder::encode)
+                .ifPresent(destination::setPassword);
     }
 }
