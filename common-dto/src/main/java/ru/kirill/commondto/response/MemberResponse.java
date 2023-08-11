17a81213514781c841e111fb68ac0255f476805b
aojona <aojona@ya.ru>
2023-08-11 18:41:39 +0300

    Add Role for Member

--- common-dto/src/main/java/ru/kirill/commondto/response/MemberResponse.java
+++ common-dto/src/main/java/ru/kirill/commondto/response/MemberResponse.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class MemberResponse {
 
     private String username;
+    private String role;
 }
---
+++ rest-api/src/main/java/ru/kirill/restapi/enums/Role.java
@@ -0,0 +1,13 @@
+package ru.kirill.restapi.enums;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER,
+    ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
