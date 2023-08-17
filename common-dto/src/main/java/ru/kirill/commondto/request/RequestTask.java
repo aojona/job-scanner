c3b4bdfec9fce9e236f69673a8fc0615fc1ab769
Kirill <aojona@ya.ru>
2023-08-18 00:35:57 +0300

    fix: add serialization support for `RequestTask`

--- common-dto/src/main/java/ru/kirill/commondto/request/RequestTask.java
+++ common-dto/src/main/java/ru/kirill/commondto/request/RequestTask.java
@@ -2,8 +2,14 @@ package ru.kirill.commondto.request;
 
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
-public class RequestTask {
+public class RequestTask implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     private Long memberId;
     private QueryParams queryParams;
