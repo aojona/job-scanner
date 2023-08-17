10f3db6cd9cde030633012eadfe837f2b2ab7b99
Kirill <aojona@ya.ru>
2023-08-17 19:47:29 +0300

    refactor: Subscription transform fields to inner class fields now

--- scheduler-service/src/main/java/ru/kirill/schedulerservice/config/MapperConfig.java
+++ scheduler-service/src/main/java/ru/kirill/schedulerservice/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package ru.kirill.schedulerservice.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,8 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return mapper;
     }
 }
