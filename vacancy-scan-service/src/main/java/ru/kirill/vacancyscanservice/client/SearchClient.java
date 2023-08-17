333bc9f5d3ff5c42dc281ceca31a468771b05a24
Kirill <aojona@ya.ru>
2023-08-17 22:35:31 +0300

    refactor: add path for url

--- vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/client/SearchClient.java
+++ vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/client/SearchClient.java
@@ -3,7 +3,7 @@ package ru.kirill.vacancyscanservice.client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import ru.kirill.commondto.response.RequestTask;
+import ru.kirill.commondto.request.RequestTask;
 import ru.kirill.commondto.response.VacancyPage;
 import ru.kirill.vacancyscanservice.config.SearchProperties;
 import ru.kirill.vacancyscanservice.util.QueryUtil;
@@ -16,7 +16,7 @@ public class SearchClient {
     private final SearchProperties searchProperties;
 
     public VacancyPage searchVacancies(RequestTask.QueryParams queryParams) {
-        String url = QueryUtil.createUrlWithParameters(searchProperties.url(), queryParams);
+        String url = QueryUtil.createUrlWithParameters(searchProperties.url(), searchProperties.path(), queryParams);
         return restTemplate
                 .getForEntity(url, VacancyPage.class)
                 .getBody();
--- vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/config/SearchProperties.java
+++ vacancy-scan-service/src/main/java/ru/kirill/vacancyscanservice/config/SearchProperties.java
@@ -3,5 +3,5 @@ package ru.kirill.vacancyscanservice.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "search")
-public record SearchProperties(String url) {
+public record SearchProperties(String url, String path) {
 }
