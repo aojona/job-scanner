73d004eeddcd889ad81d63b40d26682da3780b00
aojona <aojona@ya.ru>
2023-08-20 17:34:02 +0300

    Register `vacancy-notifier-service` on `job-server` as client

--- vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/VacancyNotifierServiceApplication.java
+++ vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/VacancyNotifierServiceApplication.java
@@ -2,12 +2,13 @@ package ru.kirill.vacancynotifierservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VacancyNotifierServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VacancyNotifierServiceApplication.class, args);
 	}
-
 }
