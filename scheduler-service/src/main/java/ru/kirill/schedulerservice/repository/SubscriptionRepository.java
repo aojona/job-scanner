5f5a68763ab259f9bbc7a08bbb469ef0284675e0
aojona <aojona@ya.ru>
2023-08-21 14:17:26 +0300

    fix "n+1" problem for `Subscription` in `scheduler-service`

--- scheduler-service/src/main/java/ru/kirill/schedulerservice/repository/SubscriptionRepository.java
+++ scheduler-service/src/main/java/ru/kirill/schedulerservice/repository/SubscriptionRepository.java
@@ -2,10 +2,12 @@ package ru.kirill.schedulerservice.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kirill.schedulerservice.entity.Subscription;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
+    @EntityGraph(attributePaths = "member")
     Slice<Subscription> findAllBy(Pageable pageable);
 }
