beff2d18431f9fdd00266eb435fe669baa845f1f
aojona <aojona@ya.ru>
2023-08-21 15:49:39 +0300

    Add and register `telegram-service` as eureka client

---
+++ telegram-service/src/test/java/ru/kirill/telegramservice/TelegramServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kirill.telegramservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
