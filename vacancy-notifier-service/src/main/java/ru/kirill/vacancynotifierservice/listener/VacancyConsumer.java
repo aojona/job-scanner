13f259f8b3724ef9e675e5af51bbba88ea8ee60b
aojona <aojona@ya.ru>
2023-08-21 13:19:43 +0300

    feat: `vacancy-notifier-service` creates notifications

--- vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/listener/VacancyConsumer.java
+++ vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/listener/VacancyConsumer.java
@@ -1,18 +1,21 @@
 package ru.kirill.vacancynotifierservice.listener;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import ru.kirill.commondto.response.VacancyResponse;
+import ru.kirill.vacancynotifierservice.service.NotificationService;
 
 @Component
 @RequiredArgsConstructor
 @RabbitListener(queues = "${rabbitmq.listen-queue}")
 public class VacancyConsumer {
 
+    private final NotificationService notificationService;
+
     @RabbitHandler
     public void handleMessage(VacancyResponse vacancyResponse) {
+        String notification = notificationService.createNotificationText(vacancyResponse.getVacancy());
     }
 }
---
+++ vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/service/NotificationService.java
@@ -0,0 +1,56 @@
+package ru.kirill.vacancynotifierservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Locale;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final MessageSource messageSource;
+    private final Locale locale = Locale.getDefault();
+
+    public <V> String createNotificationText(V v) {
+        Class<?> clazz = v.getClass();
+        String code = clazz.getSimpleName().toLowerCase();
+        StringBuilder text = new StringBuilder();
+        appendMessages(text, v, code, clazz);
+        return text.toString();
+    }
+
+    private <V> void appendMessages(StringBuilder text, V v, String code, Class<?> clazz) {
+        Arrays
+                .stream(clazz.getDeclaredFields())
+                .peek(field -> field.setAccessible(true))
+                .filter(field -> getFieldValue(v, field) != null)
+                .forEach(field -> {
+                    if (field.getType().getClassLoader () == ClassLoader.getSystemClassLoader()) {
+                        appendMessages(text, getFieldValue(v, field), code + "." + field.getName(), field.getType());
+                    } else {
+                        appendMessageIfNotBlank(text, getMessageForFieldValue(v, field, code));
+                    }
+                });
+    }
+
+    private static void appendMessageIfNotBlank(StringBuilder text, String message) {
+        text.append(message);
+        if (message != null && !message.isBlank()) {
+            text.append("\n");
+        }
+    }
+
+    private <V> String getMessageForFieldValue(V v, Field field, String code) {
+        Object[] args = {getFieldValue(v, field)};
+        return messageSource.getMessage(code + "." + field.getName(), args, "", locale);
+    }
+
+    @SneakyThrows
+    private static <V> Object getFieldValue(V v, Field field) {
+        return field.get(v);
+    }
+}
