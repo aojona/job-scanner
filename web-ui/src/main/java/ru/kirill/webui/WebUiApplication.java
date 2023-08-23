4fa0e61d7b7c1d606d8f8dceb59b824e44942f96
kirill <aojona@ya.ru>
2023-08-23 23:09:21 +0300

    Add `web-ui`

---
+++ web-ui/src/main/java/ru/kirill/webui/WebUiApplication.java
@@ -0,0 +1,15 @@
+package ru.kirill.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class WebUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUiApplication.class, args);
+	}
+
+}
