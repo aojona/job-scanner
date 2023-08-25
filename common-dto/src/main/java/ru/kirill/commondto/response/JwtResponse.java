a7e839814af27a8f20fb5fe5ca835925e38e3e9d
kirill <aojona@ya.ru>
2023-08-25 23:48:56 +0300

    refactor: add AllArgsConstructor to `JwtResponse`

--- common-dto/src/main/java/ru/kirill/commondto/response/JwtResponse.java
+++ common-dto/src/main/java/ru/kirill/commondto/response/JwtResponse.java
@@ -1,11 +1,14 @@
 package ru.kirill.commondto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtResponse {
 
     private String username;
     private String accessToken;
-    private String refreshToken;
 }
