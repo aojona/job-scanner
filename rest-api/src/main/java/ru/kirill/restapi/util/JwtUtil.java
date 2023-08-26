f3db37b0e74cbba2ad56302ca0f64dec1178e0f5
kirill <aojona@ya.ru>
2023-08-26 14:30:12 +0300

    feat: `JwtUtil` extracts data from token

--- rest-api/src/main/java/ru/kirill/restapi/util/JwtUtil.java
+++ rest-api/src/main/java/ru/kirill/restapi/util/JwtUtil.java
@@ -1,11 +1,19 @@
 package ru.kirill.restapi.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.flywaydb.core.internal.util.StringUtils;
+import ru.kirill.restapi.enums.Role;
+import java.security.Key;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @UtilityClass
 public class JwtUtil {
@@ -24,5 +32,32 @@ public class JwtUtil {
     public static Date calculateExpiration(long expirationTime, ChronoUnit expirationUnit) {
         ZonedDateTime expiration = ZonedDateTime.now().plus(expirationTime, expirationUnit);
         return Date.from(expiration.toInstant());
+    }
+
+    public static String extractUsername(@NonNull String token, @NonNull Key key) {
+        return getTokenClaims(token, key).getSubject();
+    }
+
+    public static List<Role> extractAuthorities(@NonNull String token, @NonNull Key key, @NonNull String authClaim) {
+        Claims claims = getTokenClaims(token, key);
+        return Arrays
+                .stream(claims
+                        .get(authClaim, Object.class)
+                        .toString()
+                        .replaceAll("\\pP", "")
+                        .split("\\s+")
+                )
+                .map(Role::valueOf)
+                .toList();
+    }
+
+    public static Claims getTokenClaims(@NonNull String token, @NonNull Key key) {
+        return getJwtParser(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public static JwtParser getJwtParser(Key secret) {
+        return Jwts.parserBuilder().setSigningKey(secret).build();
     }
 }
