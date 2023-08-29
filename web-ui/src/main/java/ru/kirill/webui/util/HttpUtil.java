e371b7079ced2d323ecd8660ba38964e9eb94dc2
Kirill <aojona@ya.ru>
2023-08-29 04:18:22 -0700

    fix: update `HttUtil` getHeader method

--- web-ui/src/main/java/ru/kirill/webui/util/HttpUtil.java
+++ web-ui/src/main/java/ru/kirill/webui/util/HttpUtil.java
@@ -1,17 +1,24 @@
 package ru.kirill.webui.util;
 
 import lombok.experimental.UtilityClass;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.Objects;
 
 @UtilityClass
-public class RequestUtil {
+public class HttpUtil {
 
     public String getHeader(String header) {
         return Objects
                 .requireNonNull((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
                 .getRequest()
                 .getHeader(header);
+    }
+
+    public String getHeader(ResponseEntity<?> responseEntity, String header) {
+        return responseEntity
+                .getHeaders()
+                .getFirst(header);
     }
 }
