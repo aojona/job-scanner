92b162ab1ee117f2ee7111bf08c94960469136fe
Kirill <aojona@ya.ru>
2023-08-31 15:34:37 +0300

    fix: enable cookie config properties

--- web-ui/src/main/java/ru/kirill/webui/WebUiApplication.java
+++ web-ui/src/main/java/ru/kirill/webui/WebUiApplication.java
@@ -2,10 +2,13 @@ package ru.kirill.webui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import ru.kirill.webui.config.CookieProperties;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableConfigurationProperties(CookieProperties.class)
 public class WebUiApplication {
 
 	public static void main(String[] args) {
