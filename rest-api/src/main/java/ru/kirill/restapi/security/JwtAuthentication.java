2910c486f6c67185a97c9132a7aaa8f5709b3e96
Kirill <aojona@ya.ru>
2023-09-02 20:15:39 +0300

    refactor: changed `JwtAuthentication` getPrincipal return type

--- rest-api/src/main/java/ru/kirill/restapi/security/JwtAuthentication.java
+++ rest-api/src/main/java/ru/kirill/restapi/security/JwtAuthentication.java
@@ -32,7 +32,7 @@ public class JwtAuthentication implements Authentication {
     }
 
     @Override
-    public Object getPrincipal() {
+    public JwtPrincipal getPrincipal() {
         return principal;
     }
 
