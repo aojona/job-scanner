c6112c8aa0672e4f3ef0433f5e24448712ec94e8
Kirill <aojona@ya.ru>
2023-09-04 18:46:28 +0300

    Update `web-ui` feign client request methods

--- web-ui/src/main/java/ru/kirill/webui/feign/RestApiClient.java
+++ web-ui/src/main/java/ru/kirill/webui/feign/RestApiClient.java
@@ -2,10 +2,14 @@ package ru.kirill.webui.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.kirill.commondto.request.JwtRequest;
+import ru.kirill.commondto.request.SubscriptionRequest;
 import ru.kirill.commondto.response.JwtResponse;
+import ru.kirill.commondto.response.MemberResponse;
 
 @FeignClient(name = "rest-api", path = "/api")
 public interface RestApiClient {
@@ -15,4 +19,10 @@ public interface RestApiClient {
 
     @PostMapping("/auth/join")
     ResponseEntity<JwtResponse> join(@RequestBody JwtRequest jwtRequest);
+
+    @GetMapping("/auth/member")
+    ResponseEntity<MemberResponse> getMemberFromToken();
+
+    @DeleteMapping("/subscription/delete")
+    ResponseEntity<?> deleteSubscription(@RequestBody SubscriptionRequest subscription);
 }
