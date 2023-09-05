581aaf2cefa2886070e552ffdb045507cb6775fb
Kirill <aojona@ya.ru>
2023-09-06 01:35:32 +0300

    Update `Employer` information with logo url

--- common-dto/src/main/java/ru/kirill/commondto/response/LogoUrl.java
+++ common-dto/src/main/java/ru/kirill/commondto/response/LogoUrl.java
@@ -3,20 +3,16 @@ package ru.kirill.commondto.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
+
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class Employer implements Serializable {
+public class LogoUrl implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private String id;
-    private String name;
-    private String alternateUrl;
-    private Boolean accreditedItEmployer;
-    private Boolean trusted;
-    private LogoUrl logoUrls;
+    private String original;
 }
