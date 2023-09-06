de179676119c40deb2d7b287fbaf86189a78f3aa
Kirill <aojona@ya.ru>
2023-09-06 15:16:27 +0300

    feat: `telegram-service` handles messages from telegram bot

--- telegram-service/src/main/java/ru/kirill/telegramservice/bot/TelegramBot.java
+++ telegram-service/src/main/java/ru/kirill/telegramservice/bot/TelegramBot.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.kirill.telegramservice.config.BotProperties;
 
@@ -12,14 +13,22 @@ import ru.kirill.telegramservice.config.BotProperties;
 public class TelegramBot extends TelegramLongPollingBot {
 
     private final String botUsername;
+    private final UpdateHandler updateHandler;
 
-    public TelegramBot(BotProperties botProperties) {
+    public TelegramBot(BotProperties botProperties, UpdateHandler updateHandler) {
         super(botProperties.token());
         this.botUsername = botProperties.name();
+        this.updateHandler = updateHandler;
     }
 
     @Override
     @SneakyThrows
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            SendMessage reply = updateHandler.handleMessage(update.getMessage());
+            if (reply != null) {
+                execute(reply);
+            }
+        }
     }
 }
---
+++ telegram-service/src/main/java/ru/kirill/telegramservice/bot/UpdateHandler.java
@@ -0,0 +1,37 @@
+package ru.kirill.telegramservice.bot;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import ru.kirill.telegramservice.util.MessageUtil;
+
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateHandler {
+
+    private static final String START = "/start";
+    private static final Locale LOCALE = Locale.ENGLISH;
+
+    private final MessageSource messageSource;
+
+    public SendMessage handleMessage(Message message) {
+        String data = message.getText();
+        return switch(data) {
+            case START  -> handleStartMessage(message.getChat());
+            default     -> null;
+        };
+    }
+
+    @SneakyThrows
+    private SendMessage handleStartMessage(Chat chat) {
+        Object[] messageArgs = {chat.getUserName(), chat.getId()};
+        String message = messageSource.getMessage(START, messageArgs, LOCALE);
+        return MessageUtil.createReply(chat.getId(), message);
+    }
+}
