0eb1a6d8b6ddfea5cc45d53a905375a45a40a9af
Kirill <aojona@ya.ru>
2023-09-09 19:30:55 +0300

    Add AllArgsConstructor to `ShortSubResponse`

--- common-dto/src/main/java/ru/kirill/commondto/response/ShortSubResponse.java
+++ common-dto/src/main/java/ru/kirill/commondto/response/ShortSubResponse.java
@@ -1,9 +1,13 @@
 package ru.kirill.commondto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "Подписка")
 public class ShortSubResponse {
 
