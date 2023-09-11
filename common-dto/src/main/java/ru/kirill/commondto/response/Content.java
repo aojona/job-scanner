83a8181ea5825664e47b9d5922c11491517cb127
Kirill <aojona@ya.ru>
2023-09-11 19:14:06 +0300

    Add default constructor to `Content` dto for properly json parsing

--- common-dto/src/main/java/ru/kirill/commondto/response/Content.java
+++ common-dto/src/main/java/ru/kirill/commondto/response/Content.java
@@ -2,10 +2,12 @@ package ru.kirill.commondto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Content<C> {
     private List<C> content;
