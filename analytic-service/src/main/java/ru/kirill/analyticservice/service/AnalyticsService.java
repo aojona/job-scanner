77a6467100f936ae6ac63865751ab4aab9b26aa5
Kirill <aojona@ya.ru>
2023-09-11 15:04:23 +0300

    feat: `AnalyticService` gathers vacancy statistics for the current day

---
+++ analytic-service/src/main/java/ru/kirill/analyticservice/service/AnalyticsService.java
@@ -0,0 +1,35 @@
+package ru.kirill.analyticservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import ru.kirill.analyticservice.entity.VacancyRedis;
+import ru.kirill.analyticservice.util.VacancyAnalyzer;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class AnalyticsService {
+
+    private final VacancyService vacancyService;
+
+    public VacancyAnalyzer gatherStatistics(String query) {
+        LocalDate currentDate = LocalDate.now();
+        VacancyAnalyzer analyzer = new VacancyAnalyzer();
+        Page<VacancyRedis> vacancies = vacancyService.findVacancies(currentDate, query);
+        analyzeData(analyzer, vacancies);
+        while (vacancies.hasNext()) {
+            vacancies = vacancyService.findVacancies(currentDate, query, vacancies.nextPageable());
+            analyzeData(analyzer, vacancies);
+        }
+        return analyzer;
+    }
+
+    private void analyzeData(VacancyAnalyzer analyzer, Page<VacancyRedis> vacancies) {
+        vacancies
+                .stream()
+                .map(VacancyRedis::getSalary)
+                .forEach(analyzer::analyze);
+    }
+}
---
+++ analytic-service/src/main/java/ru/kirill/analyticservice/util/VacancyAnalyzer.java
@@ -0,0 +1,29 @@
+package ru.kirill.analyticservice.util;
+
+import lombok.Data;
+import ru.kirill.commondto.response.Salary;
+
+@Data
+public class VacancyAnalyzer {
+
+    private double averageMinSalary;
+    private double averageMaxSalary;
+    private int numberOfVacancies;
+    private int numberOfVacanciesWithSalary;
+
+    public void analyze(Salary salary) {
+        numberOfVacancies++;
+        if (salary != null) {
+            numberOfVacanciesWithSalary++;
+            averageMinSalary = calculateAvgSalary(averageMinSalary, salary.getFrom());
+            averageMaxSalary = calculateAvgSalary(averageMaxSalary, salary.getTo());
+        }
+    }
+
+    private double calculateAvgSalary(Double averageSalary, Integer salary) {
+        if (salary != null) {
+            averageSalary = averageSalary + (salary - averageSalary) / numberOfVacancies;
+        }
+        return averageSalary;
+    }
+}
