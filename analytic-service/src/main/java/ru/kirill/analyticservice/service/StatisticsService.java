d9451015c68d54cf3203291b8b53671efaf3c2b4
Kirill <aojona@ya.ru>
2023-09-15 14:53:53 +0300

    refactor: `StatisticsService` updates already existing statistics

--- analytic-service/src/main/java/ru/kirill/analyticservice/service/StatisticsService.java
+++ analytic-service/src/main/java/ru/kirill/analyticservice/service/StatisticsService.java
@@ -3,6 +3,7 @@ package ru.kirill.analyticservice.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kirill.analyticservice.entity.VacancyStatistics;
 import ru.kirill.analyticservice.repository.StatisticsRepository;
 import ru.kirill.analyticservice.util.VacancyAnalyzer;
@@ -14,7 +15,15 @@ public class StatisticsService {
     private final StatisticsRepository statisticsRepository;
     private final ModelMapper modelMapper;
 
+    @Transactional
     public void save(VacancyAnalyzer analyzer) {
-        statisticsRepository.save(modelMapper.map(analyzer, VacancyStatistics.class));
+        VacancyStatistics vacancyStatistics = statisticsRepository
+                .findByQueryAndDate(analyzer.getQuery(), analyzer.getDate())
+                .map(statistics -> {
+                    modelMapper.map(analyzer, statistics);
+                    return statistics;
+                })
+                .orElse(modelMapper.map(analyzer, VacancyStatistics.class));
+        statisticsRepository.save(vacancyStatistics);
     }
 }
