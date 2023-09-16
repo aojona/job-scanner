56bc6ce49aea308c2c77eb737c045fe4b1a7deb5
Kirill <aojona@ya.ru>
2023-09-16 13:22:58 +0300

    add findByQueryAndDate method to `StatisticsRepository`

--- analytic-service/src/main/java/ru/kirill/analyticservice/repository/StatisticsRepository.java
+++ analytic-service/src/main/java/ru/kirill/analyticservice/repository/StatisticsRepository.java
@@ -3,5 +3,10 @@ package ru.kirill.analyticservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.kirill.analyticservice.entity.VacancyStatistics;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface StatisticsRepository extends JpaRepository<VacancyStatistics, Long> {
+
+    Optional<VacancyStatistics> findByQueryAndDate(String query, LocalDate date);
 }
