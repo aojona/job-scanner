abd3bd7eda159d681144b7ae6a0a215ea5d91d10
Kirill <aojona@ya.ru>
2023-09-16 13:18:36 +0300

    fix: add findRandomSubscriptions method to `SubscriptionRepository`

--- rest-api/src/main/java/ru/kirill/restapi/repository/SubscriptionRepository.java
+++ rest-api/src/main/java/ru/kirill/restapi/repository/SubscriptionRepository.java
@@ -1,12 +1,19 @@
 package ru.kirill.restapi.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.kirill.restapi.entity.Subscription;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
     @EntityGraph(attributePaths = "members")
     Optional<Subscription> findByText(String text);
+
+    @Query("SELECT s FROM Subscription s ORDER BY RANDOM()")
+    List<Subscription> findRandomSubscriptions(Pageable pageable);
 }
