dc6f37da1b4f52fedd7de46472561bcbcb9b764e
Kirill <aojona@ya.ru>
2023-09-17 12:35:17 +0300

    fix: removed delay

--- vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/listener/VacancyConsumer.java
+++ vacancy-notifier-service/src/main/java/ru/kirill/vacancynotifierservice/listener/VacancyConsumer.java
@@ -28,11 +28,6 @@ public class VacancyConsumer {
                             vacancyResponse.getQueryText()
                     );
                     template.convertAndSend(notification);
-                    try {
-                        Thread.sleep(20_000L);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
                 });
     }
 }
