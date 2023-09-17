3651c947c368a60eb9e9b41a4e1a3f8a1f0a58e1
Kirill <aojona@ya.ru>
2023-09-17 10:51:17 +0300

    refactor: `TelegramBot` handles exceptions when unable to send message to member

--- telegram-service/src/main/java/ru/kirill/telegramservice/bot/TelegramBot.java
+++ telegram-service/src/main/java/ru/kirill/telegramservice/bot/TelegramBot.java
@@ -2,15 +2,18 @@ package ru.kirill.telegramservice.bot;
 
 import lombok.Getter;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.kirill.commondto.response.Notification;
 import ru.kirill.telegramservice.config.BotProperties;
 import ru.kirill.telegramservice.util.MessageUtil;
 
+@Slf4j
 @Getter
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -30,19 +33,30 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             SendMessage reply = updateHandler.handleMessage(update.getMessage());
             if (reply != null) {
-                execute(reply);
+                try {
+                    execute(reply);
+                } catch (TelegramApiException e) {
+                    log.error("Error while sending message with image to chat with id = " + reply.getChatId());
+                }
             }
         }
     }
 
-    @SneakyThrows
     public void sendNotification(Notification notification) {
         if (notification.getLogoUrl() != null && !notification.getLogoUrl().contains("bmp")) {
             SendPhoto reply = MessageUtil.createReply(notification.getChatId(), notification.getText(), notification.getLogoUrl());
-            execute(reply);
+            try {
+                execute(reply);
+            } catch (TelegramApiException e) {
+                log.error("Error while sending message with image to chat with id = " + notification.getChatId());
+            }
         } else {
             SendMessage reply = MessageUtil.createReply(notification.getChatId(), notification.getText());
-            execute(reply);
+            try {
+                execute(reply);
+            } catch (TelegramApiException e) {
+                log.error("Error while sending text message to chat with id = " + notification.getChatId());
+            }
         }
     }
 }
