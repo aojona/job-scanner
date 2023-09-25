a180e71cf8b419656533bd44da60391ab3197584
Kirill <aojona@ya.ru>
2023-09-25 17:05:34 +0300

    Adapt `web-ui` interface for mobile phones

--- web-ui/src/main/resources/static/javascript/mainCharts.js
+++ web-ui/src/main/resources/static/javascript/mainCharts.js
@@ -1,80 +1,93 @@
-const ctx1 = document.getElementById('averageSalaryRangeChart');
-const ctx2 = document.getElementById('vacancyNumberCharts');
+const font = new FontFace("Poppins", "https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap");
+document.fonts.add(font);
+font.load();
+document.fonts.ready.then(() => {
 
-Chart.defaults.font.family = "Poppins";
-Chart.defaults.font.size = 16;
-Chart.defaults.color = "#3c4fe0";
+    const ctx1 = document.getElementById('averageSalaryRangeChart');
+    ctx1.height = 200;
+    const ctx2 = document.getElementById('vacancyNumberCharts');
+    ctx2.height = 200;
 
-function createData(firstData, firstLabel, secondData, secondLabel) {
-    return {
-        labels: query,
-        datasets: [{
-            label: firstLabel,
-            data: firstData,
-            backgroundColor: [
-                'rgba(255,120,171,0.05)',
-            ],
-            borderColor: [
-                '#ff69a4',
-            ],
-            borderWidth: 1.5
-        },{
-            label: secondLabel,
-            data: secondData,
-            backgroundColor: [
-                'rgb(79,93,252, 0.05)'
-            ],
-            borderColor: [
-                '#4359ff'
-            ],
-            borderWidth: 1.5
-        },]
-    };
-}
+    Chart.defaults.font.family = "Poppins";
+    Chart.defaults.font.size = 16;
+    Chart.defaults.color = "#3c4fe0";
 
-function createConfig(data, text) {
-    return {
-        type: 'bar',
-        data,
-        options: {
-            scales: {
-                x: {
-                    stacked: true,
-                    grid: {
-                        display: false
+    function createData(firstData, firstLabel, secondData, secondLabel) {
+        return {
+            labels: query,
+            datasets: [{
+                label: firstLabel,
+                data: firstData,
+                backgroundColor: [
+                    'rgba(255,120,171,0.05)',
+                ],
+                borderColor: [
+                    '#ff69a4',
+                ],
+                borderWidth: 1.5
+            }, {
+                label: secondLabel,
+                data: secondData,
+                backgroundColor: [
+                    'rgb(79,93,252, 0.05)'
+                ],
+                borderColor: [
+                    '#4359ff'
+                ],
+                borderWidth: 1.5
+            },]
+        };
+    }
+
+    function createConfig(data, text) {
+        return {
+            type: 'bar',
+            data,
+            options: {
+                scales: {
+                    x: {
+                        stacked: true,
+                        grid: {
+                            display: false
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            maxRotation: 90,
+                            minRotation: 90
+                        },
                     }
                 },
-                y: {
-                    beginAtZero: true,
+                plugins: {
                     title: {
                         display: true,
-                        maxRotation: 90,
-                        minRotation: 90
+                        text: text,
+                        font: {
+                            weight: 'normal',
+                            size: 18,
+                        }
                     },
-                }
-            },
-            plugins: {
-                title: {
-                    display: true,
-                    text: text,
-                    font: {
-                        size: 18,
+                    legend: {
+                        labels: {
+                            usePointStyle: true,
+                            boxHeight: 9,
+                            font: {
+                                weight: 300
+                            }
+                        }
                     }
                 },
-                legend: {
-                    labels: {
-                        color: "#e03d7b",
-                    }
-                }
             }
-        }
-    };
-}
+        };
+    }
 
-var data = createData(averageMinSalary, 'from', averageMaxSalary, 'to');
-var config = createConfig(data, 'Average salary range for random subscriptions');
-const averageSalaryRangeChart = new Chart(ctx1, config);
+    var data = createData(averageMinSalary, 'from', averageMaxSalary, 'to');
+    var config = createConfig(data, '        Average salary range');
+    const averageSalaryRangeChart = new Chart(ctx1, config);
 
-data = createData(numberOfVacanciesWithSalary, 'with salary range', numberOfVacancies, 'all')
-config = createConfig(data, 'Number of vacancies for random subscriptions')
-const vacancyNumberCharts = new Chart(ctx2, config);
+    data = createData(numberOfVacanciesWithSalary, 'with salary range', numberOfVacancies, 'all')
+    config = createConfig(data, '        Number of vacancies')
+    const vacancyNumberCharts = new Chart(ctx2, config);
+});
--- web-ui/src/main/resources/static/javascript/memberCharts.js
+++ web-ui/src/main/resources/static/javascript/memberCharts.js
@@ -1,90 +1,128 @@
-const ctx1 = document.getElementById('numberOfVacancies');
-const ctx2 = document.getElementById('averageMinSalary');
-const ctx3 = document.getElementById('averageMaxSalary');
+const font = new FontFace("Poppins", "https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap");
+document.fonts.add(font);
+font.load();
+document.fonts.ready.then(() => {
 
+    const ctx1 = document.getElementById('numberOfVacancies');
+    ctx1.height = 200;
+    const ctx2 = document.getElementById('averageMinSalary');
+    ctx2.height = 200;
+    const ctx3 = document.getElementById('averageMaxSalary');
+    ctx3.height = 200;
 
-Chart.defaults.font.family = "Poppins";
-Chart.defaults.font.size = 16;
-Chart.defaults.color = "#3c4fe0";
+    Chart.defaults.font.family = "Poppins";
+    Chart.defaults.font.size = 16;
+    Chart.defaults.color = "#3c4fe0";
 
-function createData(date, datasets) {
-    return {
-        labels: date,
-        datasets: datasets
-    };
-}
-
-function createConfig(data, text) {
-    return {
-        type: 'line',
-        data,
-        options: {
-            layout: {
-                padding: {
-                    left: 50,
-                    right: 50
-                }
-            },
-            scales: {
-                x: {
-                    stacked: true,
-                    grid: {
-                        display: false
-                    },
-                    reverse: true
-                },
-            },
-            plugins: {
-                title: {
-                    display: true,
-                    text: text,
-                    font: {
-                        size: 18,
-                    }
-                },
-                legend: {
-                    labels: {
-                        color: "#e03d7b",
-                    }
-                }
-            }
-        }
-    };
-}
-
-var index = 0
-var maxDateRange = allDate[0]
-for (i = 1; i < allDate.length; i++) {
-    if (allDate[1].length > maxDateRange) {
-        index = i;
-        maxDateRange = allDate[i];
+    function createData(date, datasets) {
+        return {
+            labels: date,
+            datasets: datasets
+        };
     }
-}
 
-var colors = ["#ff0062", "#003dff", "#ff8000", "#f002ff"]
-var backgroundColor = ["rgba(255,0,98,0.05)", "rgba(0,61,255,0.05)", "rgba(255,128,0,0.1)", "rgba(240,2,255,0.1)"]
+    function createConfig(data, text) {
+        return {
+            type: 'line',
+            data,
+            options: {
+                responsive: true,
+                layout: {
+                    padding: {
+                        left: 15,
+                        right: 50
+                    }
+                },
+                scales: {
+                    x: {
+                        stacked: true,
+                        grid: {
+                            display: false
+                        },
+                        reverse: true
+                    },
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: text,
+                        font: {
+                            weight: 'normal',
+                            size: 18,
+                        }
+                    },
+                    legend: {
+                        labels: {
+                            usePointStyle: true,
+                            boxHeight: 9,
+                            font: {
+                                weight: 300
+                            }
+                        },
+                    }
+                },
+            }
+        };
+    }
 
-var datasets = []
-for (i = 0; i < query.length; i++) {
-    datasets.push({label: query[i], data: allNumberOfVacancies[i], borderColor: colors[i], fill: true, tension: 0.1, borderWidth: 1.5, backgroundColor: backgroundColor[i]})
-}
+    var index = 0
+    var maxDateRange = allDate[0].length
+    for (i = 1; i < allDate.length; i++) {
+        if (allDate[i].length > maxDateRange) {
+            index = i;
+            maxDateRange = allDate[i];
+        }
+    }
 
-var data = createData(allDate[index], datasets);
-var config = createConfig(data, 'Number of vacancies');
-const numberOfVacanciesChart = new Chart(ctx1, config);
+    var colors = ["#ff0062", "#003dff", "#ff8000", "#f002ff"]
+    var backgroundColor = ["rgba(255,0,98,0.05)", "rgba(0,61,255,0.05)", "rgba(255,128,0,0.1)", "rgba(240,2,255,0.1)"]
 
-datasets = [];
-for (i = 0; i < query.length; i++) {
-    datasets.push({label: query[i], data: allAverageMinSalary[i], borderColor: colors[i], fill: true, tension: 0.1, borderWidth: 1.5, backgroundColor: backgroundColor[i]});
-}
-data = createData(allDate[index], datasets);
-config = createConfig(data, 'Average minimum salary');
-const averageMinSalaryChart = new Chart(ctx2, config);
+    var datasets = []
+    for (i = 0; i < query.length; i++) {
+        datasets.push({
+            label: query[i],
+            data: allNumberOfVacancies[i],
+            borderColor: colors[i],
+            fill: true,
+            tension: 0.1,
+            borderWidth: 1.5,
+            backgroundColor: backgroundColor[i]
+        })
+    }
 
-datasets = [];
-for (i = 0; i < query.length; i++) {
-    datasets.push({label: query[i], data: allAverageMaxSalary[i], borderColor: colors[i], fill: true, tension: 0.1, borderWidth: 1.5, backgroundColor: backgroundColor[i]});
-}
-data = createData(allDate[index], datasets);
-config = createConfig(data, 'Average maximum salary');
-const averageMaxSalaryChart = new Chart(ctx3, config);
+    var data = createData(allDate[index], datasets);
+    var config = createConfig(data, '         Number of vacancies');
+    const numberOfVacanciesChart = new Chart(ctx1, config);
+
+    datasets = [];
+    for (i = 0; i < query.length; i++) {
+        datasets.push({
+            label: query[i],
+            data: allAverageMinSalary[i],
+            borderColor: colors[i],
+            fill: true,
+            tension: 0.1,
+            borderWidth: 1.5,
+            backgroundColor: backgroundColor[i]
+        });
+    }
+    data = createData(allDate[index], datasets);
+    config = createConfig(data, '         Average minimum salary');
+    const averageMinSalaryChart = new Chart(ctx2, config);
+
+    datasets = [];
+    for (i = 0; i < query.length; i++) {
+        datasets.push({
+            label: query[i],
+            data: allAverageMaxSalary[i],
+            borderColor: colors[i],
+            fill: true,
+            tension: 0.1,
+            borderWidth: 1.5,
+            backgroundColor: backgroundColor[i]
+        });
+    }
+    data = createData(allDate[index], datasets);
+    config = createConfig(data, '         Average maximum salary');
+    const averageMaxSalaryChart = new Chart(ctx3, config);
+});
