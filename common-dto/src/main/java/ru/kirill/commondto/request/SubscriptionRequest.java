ad4fec4f071a5c2dc98db8e27fa5354d6fe4ebe6
Kirill <aojona@ya.ru>
2023-09-29 21:32:33 +0300

    update `SubscriptionRequest` validation

--- common-dto/src/main/java/ru/kirill/commondto/request/SubscriptionRequest.java
+++ common-dto/src/main/java/ru/kirill/commondto/request/SubscriptionRequest.java
@@ -1,6 +1,7 @@
 package ru.kirill.commondto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ public class SubscriptionRequest {
     @Schema(description = "Query", example = "Java developer")
     @Size(min = 3, message = "{error.subscription.short}")
     @Size(max = 32, message = "{error.subscription.long}")
+    @Pattern(regexp = "^\\p{Lu}.*$", message = "{error.subscription.letter}")
     private String text;
 
     @Schema(description = "member id", example = "1")
